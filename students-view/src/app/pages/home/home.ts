4ec714ade8137dab34c96173e26993deca03e4b2
fungirox <fungirox@proton.me>
2026-01-22 12:42:21 -0700

    Typos mistake

--- students-view/src/app/pages/home/home.ts
+++ students-view/src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GetRequest } from '../../services/get-request';
+import { GetRequest } from '../../services/read/get-request';
 import { RouterLink } from "@angular/router";
 
 @Component({
--- students-view/src/app/pages/student/student-show/student-show.ts
+++ students-view/src/app/pages/student/student-show/student-show.ts
@@ -42,3 +42,4 @@ export class StudentShow implements OnInit {
 
   }
 }
+ 
