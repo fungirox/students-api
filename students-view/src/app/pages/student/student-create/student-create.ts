17583e616300ae1c9f178fef50147ca20af70f2b
fungirox <fungirox@proton.me>
2026-01-26 12:19:44 -0700

    Validación en Create Student e instalación de CDK

--- students-view/src/app/pages/student/student-create/student-create.ts
+++ students-view/src/app/pages/student/student-create/student-create.ts
@@ -1,37 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Form, FormBuilder, FormGroup, FormsModule, NonNullableFormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { PostRequest } from '../../../services/create/post-request';
 import { Student } from '../../../interface/student';
 
 @Component({
   selector: 'app-student-create',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './student-create.html',
   styleUrl: './student-create.css',
 })
 export class StudentCreate {
-    student : Student = {
-    'id': '',
-    'first_name': '',
-    'middle_name': '',
-    'last_name': '',
-    'gender': 'f'
-  };
 
   constructor(
     public post_request: PostRequest,
-    private new_route : Router
+    private new_route: Router
   ) { }
 
-  createStudent(){
-    this.post_request.createStudent(this.student).subscribe({
+  fb = inject(NonNullableFormBuilder);
+  student_form = this.fb.group({
+    first_name: ['', { validators: [Validators.required, Validators.minLength(2), Validators.maxLength(45)] }],
+    middle_name: ['', { validators: [Validators.minLength(2), Validators.maxLength(45)] }],
+    last_name: ['', { validators: [Validators.required, Validators.minLength(2), Validators.maxLength(45)] }],
+    gender: ['f']
+  });
+
+  createStudent() {
+    this.post_request.createStudent(this.student_form.value).subscribe({
       next: (response: any) => {
-        const student = response.student
-        this.new_route.navigate([`/student/show/${student["id"]}`]);
+        const student_id = response.student.id;
+        this.student_form.patchValue(response.student);
+        this.new_route.navigate([`/student/show/${student_id}`]);
       },
       error: (error) => console.error(error)
     });
   }
 }
-  
